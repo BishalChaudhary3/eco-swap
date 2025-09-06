026dae1e26b58735fe4f3121a85a986886ab0a4c
Dikshyant-Lamsal <140510854+Dikshyant-Lamsal@users.noreply.github.com>
2025-09-06 11:37:15 +0530

    Update footer copyright notice to include trademark symbol

--- eco-swap-fe/src/components/Footer.jsx
+++ eco-swap-fe/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-gray-200 p-4 text-centerr">
-      <p>&copy; {new Date().getFullYear()} EcoFinds</p>
+      <p>&copy; {new Date().getFullYear()} EcoFinds TM</p>
     </footer>
   );
 }
